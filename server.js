581997fdccdb9816c9430d7bdba41c8b42da88db
avdkishore <avdkishore@gmail.com>
2019-11-08 12:50:31 +0530

    Setup custom server to handle routes

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const next = require('next');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.APP_ENV === 'local';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/articles/new', (req, res) => {
+    const actualPage = '/articles/edit';
+    app.render(req, res, actualPage);
+  });
+
+  server.get('/articles/edit/:id', (req, res) => {
+    console.log('req.params', req.params)
+    const actualPage = '/articles/edit';
+    const queryParams = { id: req.params.id };
+    app.render(req, res, actualPage, queryParams);
+  });
+
+  server.get('/articles/:id', (req, res) => {
+    const actualPage = '/articles';
+    const queryParams = { id: req.params.id };
+    app.render(req, res, actualPage, queryParams);
+  });
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+
+    console.log(`> Ready on http://localhost:${port}`); // eslint-disable-line
+  });
+});
