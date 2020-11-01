5a72dd6076bca8da3ced153a32facf48318ba459
avdkishore <avdkishore@gmail.com>
2020-11-01 12:14:07 +0530

    debug firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -10,6 +10,8 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
+console.log('firebase config => ', firebaseConfig);
+
 try {
   firebase.initializeApp(firebaseConfig);
 } catch(err){
